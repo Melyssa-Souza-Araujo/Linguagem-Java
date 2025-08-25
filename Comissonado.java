b00f7f533aefc01f77a9c23df35403df5a85a5eb
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-08-25 11:19:24 -0300

    Update Comissonado.java

--- Comissonado.java
+++ Comissonado.java
@@ -1,18 +1,17 @@
 package exe05;
 
-public class Comissonado extends Empregado {
-	private double totalVenda;
-	public double getTotalVenda() {
-		return totalVenda;
-	}
-	public void setTotalVenda(double totalVenda) {
-		this.totalVenda = totalVenda;
-	}
-	public double getTaxaComissao() {
-		return taxaComissao;
-	}
-	public void setTaxaComissao(double taxaComissao) {
-		this.taxaComissao = taxaComissao;
-	}
-	private double taxaComissao;
+public class Comissionado extends Empregado {
+    private double totalVenda;
+    private double taxaComissao;
+
+    public Comissionado(String nome, String sobrenome, String cpf, double totalVenda, double taxaComissao) {
+        super(nome, sobrenome, cpf);
+        this.totalVenda = totalVenda;
+        this.taxaComissao = taxaComissao;
+    }
+
+    @Override
+    public double calcularVencimento() {
+        return totalVenda * taxaComissao;
+    }
 }
