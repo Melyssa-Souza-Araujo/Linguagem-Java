4d1cbc6098cc962029d92fa97967ab64f893b815
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-08-25 11:17:28 -0300

    Update PessoaFisica.java

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,5 +1,16 @@
 package exe06;
 
 public class PessoaFisica extends Pessoa {
-	
+    private String cpf;
+
+    public PessoaFisica(String nome, String cpf) {
+        super(nome);
+        this.cpf = cpf;
+    }
+
+    @Override
+    public void calcularImpostoRenda() {
+        System.out.println("Calculando imposto de renda para Pessoa Física: " + nome);
+        // lógica específica para PF
+    }
 }
