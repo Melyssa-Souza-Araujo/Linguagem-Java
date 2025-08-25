9a169e0f9d164a2b77a7497edfa82dbb35884ffe
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-08-25 10:09:38 -0300

    Update Empregado.java

--- Empregado.java
+++ Empregado.java
@@ -1,32 +1,15 @@
 package exe05;
 
 public abstract class Empregado {
-	private String nome;
-	private String sobrenome;
-	private String cpf;
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getSobrenome() {
-		return sobrenome;
-	}
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public double calcularVencimento() {
-		return 0;
-	}
-	
-	
+    protected String nome;
+    protected String sobrenome;
+    protected String cpf;
+
+    public Empregado(String nome, String sobrenome, String cpf) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cpf = cpf;
+    }
+
+    public abstract double calcularVencimento();
 }
