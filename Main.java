91078c36d25c68b0f0fdd5e0c0d113d49f09f5c1
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-08-25 13:17:52 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package exe08;
+
+public class Main {
+    public static void main(String[] args) {
+        Pi p1 = new Pi();
+
+        System.out.println("Double: " + p1.getPI());
+        System.out.println("Int....: " + p1.getPIInt());
+        System.out.println("Float..: " + p1.getPIFloat());
+    }
+}
