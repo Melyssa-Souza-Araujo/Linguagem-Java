7d53424ed1f3f47a950ad9d2f93e5967a49522bd
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-09-01 09:12:05 -0300

    Create ContaBancaria02.java

---
+++ ContaBancaria02.java
@@ -0,0 +1,33 @@
+package atividade;
+
+public class ContaBancaria {
+    protected String numeroConta;
+    protected double saldo;
+
+    public ContaBancaria(String numeroConta, double saldoInicial) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldoInicial;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    public boolean sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public void exibirSaldo() {
+        System.out.printf("Conta %s - Saldo: R$ %.2f\n", numeroConta, saldo);
+    }
+}
