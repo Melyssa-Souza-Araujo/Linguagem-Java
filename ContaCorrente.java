6d7f79cbc969d0974934ea90620db9ae5825a990
MELYSSA SOUZA ARAUJO <126412768+Melyssa-Souza-Araujo@users.noreply.github.com>
2025-09-01 09:13:41 -0300

    Create ContaCorrente.java

---
+++ ContaCorrente.java
@@ -0,0 +1,9 @@
+package atividade;
+
+public class ContaCorrente extends ContaBancaria {
+
+    public ContaCorrente(String numeroConta, double saldoInicial) {
+        super(numeroConta, saldoInicial);
+    }
+
+}
